e9cb64f6b56beda95bdd2e7d1d2d2c5adcb47d30
Sanjai-3101 <shasan4467@gmail.com>
2026-07-06 17:18:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,40 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+  react(),
+  tailwindcss(),
+
+  VitePWA({
+    registerType: 'autoUpdate',
+
+    manifest: {
+      name: 'VantageAI',
+      short_name: 'VantageAI',
+      description: 'AI Travel Planner',
+      theme_color: '#2563eb',
+      background_color: '#ffffff',
+      display: 'standalone',
+
+      icons: [
+        {
+          src: 'icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  }),
+],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
