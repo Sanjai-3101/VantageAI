2beefb99333ce39e93a561f491463e8728a7cabf
Sanjai-3101 <shasan4467@gmail.com>
2026-07-06 17:07:51 +0530

    Delete public/public/sw.js

--- public/public/sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker Installed");
-});
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
-});
